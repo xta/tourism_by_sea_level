ad83d6c0b7c10528fd7209ecc25dabbf0e08383e
Rex Feng <rexfeng@gmail.com>
2014-07-19 23:29:34 -0400

    add initial locations.json collection from geocoder lib

---
+++ assets/js/lib/geocoder.js
@@ -0,0 +1,50 @@
+// file system & helpers
+var fs            = require('fs');
+var destinations  = require('../destinations.json');
+var outputFile    = './assets/js/locations.json';
+var _             = require('underscore');
+
+// geocoder
+var geocoderProvider  = 'openstreetmap',
+    httpAdapter       = 'http',
+    geocoder          = require('node-geocoder').getGeocoder(geocoderProvider, httpAdapter);
+
+// internal geocoder states
+var locations,
+    id = 0,
+    location_name,
+    location_country,
+    location_lat,
+    location_lng;//,
+    // geo_response;
+
+  locations = _.map(destinations, function(destination){
+
+  id++;
+  location_name     = destination.split("(")[0].trim();
+  location_country  = destination.split("(")[1].slice(0, - 1);
+
+  // geo_response = geocoder.geocode(location_name, function(err, res) {
+  //   if(err) {
+  //     console.log(err);
+  //   } else {
+  //     return res;
+  //   }
+  // });
+
+  return {
+    id: id,
+    name: location_name,
+    country: location_country//,
+    // lat: geo_response.latitude,
+    // lng: geo_response.longitude
+  };
+});
+
+fs.writeFile(outputFile, JSON.stringify(locations, null, 4), function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("JSON saved to " + outputFile);
+  }
+});
